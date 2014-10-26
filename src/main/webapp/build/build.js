6aeecc78a2bf8a498768bbf46e3c13b003d3fc35
moss0801 <moss0801@gmail.com>
2014-10-26 21:24:49 +0900

    [수정] requirejs를 포함해서 build하도록 수정함

--- src/main/webapp/build/build.js
+++ src/main/webapp/build/build.js
@@ -1,6 +1,7 @@
 ({
   baseUrl : "../app",
   paths: {
+    'requirejs': '../bower_components/requirejs/require',
     'text': '../bower_components/requirejs-text/text',
     'angular': '../bower_components/angular/angular',
     'angular-ui-router': '../bower_components/angular-ui-router/release/angular-ui-router'
@@ -14,6 +15,7 @@
     }
   },
   name: "js/main",
+  include: 'requirejs',
   out: "../dist/main.js",
   removeCombined: true
 })
