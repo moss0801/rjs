9cafbcce2592447745a9d4d954b49cf9874dd818
moss0801 <moss0801@gmail.com>
2014-11-27 00:38:12 +0900

    [수정] 주석 수정

--- src/main/webapp/app/js/directives/rjsDynamicColumn.js
+++ src/main/webapp/app/js/directives/rjsDynamicColumn.js
@@ -16,11 +16,13 @@
                   alert(value);
                 };
                 
+                // compile time에 template을 로드할 수 있도록 templateUrl에 지정 후, 다시 로드
                 var template = $templateCache.get('app/templates/rjsDynamicColumn.html');
                 var templateElement = angular.element(template[1]);
+                // 기존 template 제거
+                element.html('');
                 
                 // template 구성
-                element.html('');
                 for (var i = 0; i < scope.columnList.length; i++) {
                   for (var j = 0; j < templateElement.length; j += 2) {
                     var target = angular.element(templateElement[j]);
