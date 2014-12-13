a1a0bf8d5a1365e7e02fe10ec2f064ae8cebcd70
moss0801 <moss0801@gmail.com>
2014-12-14 03:39:36 +0900

    [수정] 탭 200개에 editor를 붙여봄

--- src/main/webapp/app/js/controllers/manyTabController.js
+++ src/main/webapp/app/js/controllers/manyTabController.js
@@ -22,15 +22,18 @@
         }
         $scope.tabList = list;
         
+        $scope.tinymceOptions = {
+          
+        };
+        
         $scope.addTab = function() {
           count++;
           $scope.tabList.push({
             title: 'title' + count,
             tootle: 'tooltip' + count,
-            content: 'content' + count + 'content' + count + 'content' + count + 'content' + count          
+            content: 'content' + count + 'content' + count + 'content' + count + 'content' + count
           });
         };
-      
     }];
   });
 })(define);
